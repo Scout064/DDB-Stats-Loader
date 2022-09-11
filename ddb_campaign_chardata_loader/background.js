e989559de06fda5bcec85ab8e2bc59ef87458c74
Scout064 <78973583+Scout064@users.noreply.github.com>
2022-09-11 21:51:11 +0200

    Add files via upload

--- ddb_campaign_chardata_loader/background.js
+++ ddb_campaign_chardata_loader/background.js
@@ -3,14 +3,30 @@
 // add listener for URL array
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
-    var ddb_url_array = [];
+    var ddb_urls_array = [];
 // if valid data received, print to console and
 // parse received data to variable
-//    if (ddb_char_array != 'undefined' ) {
     if (request.message == 'url_array' ) {
         ddb_url_array = request.data;
         console.log('received url data! ' + '... ' + 'printing to console!');
         console.log(ddb_url_array);}
     else {
         console.log('NO URL DATA RECEIVED!');}
+    });
+// add listener for character array
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+    var ddb_char_array = [];
+    // define function to get character urls
+    function requestCharData () {
+// if valid data received, print to console and
+// parse received data to variable
+    if (request.message == 'data_array' ) {
+        ddb_char_array = request.data;
+        console.log('received char data! ' + '... ' + 'printing to console!');
+        console.log(ddb_char_array);}
+    else {
+        console.log('NO CHARACTER DATA RECEIVED!');}
+        }
+    setInterval(requestCharData, 30000);
     });
