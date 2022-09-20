c4d2ae73f685b0b8bd5d4f2a94513436fe3283d1
Scout064 <78973583+Scout064@users.noreply.github.com>
2022-09-20 15:19:51 +0200

    Update main_script_pipico.py

--- ddb_ccl_CLIENT/main_script_pipico.py
+++ ddb_ccl_CLIENT/main_script_pipico.py
@@ -1,225 +1,101 @@
-## Multi Core Program example
-led_red = machine.Pin(0, machine.Pin.OUT)
-led_green = machine.Pin(1, machine.Pin.OUT)
-
-sLock = thread.allocate_lock()
-
-def CoreTask():
-    while True:
-        sLock.acquire()
-        print("Enter second Thred")
-        sleep(1)
-        led_green.high()
-        print("Green LED is turned ON")
-        sleep(2)
-        led_green.low()
-        print("Green LED is turned OFF")
-        sleep(1)
-        print("Exit second Thread")
-        sleep(1)
-        sLock.release()
-thread.start_new_thread(CoreTask, ())
-
-while True:
-    sLock.acquire()
-    print("Enter main Thread")
-    led_red.toggle()
-    sleep(0.15)
-    print("Red LED toggling...")
-    sleep(1)
-    print("Exit main Thread")
-    sleep(1)
-    sLock.release()
-
-#######################################################
-#######################################################
-
 ## Import libraries
 import machine
 import _thread as thread
-import ujson as json
+import json
 import picoweb
 from time import sleep
 import network
 import ure as re
 import ulogging as logging
-import uasyncio as asyncio
+import uasyncio
+from tinydb import TinyDB, Query
+import utemplate
+import neopixel
 
 ## Network Setup 
-ssid = '*****'
-password = '*******'
+#ssid = 'IOT'
+#password = 'T79brWDLcZp3LVrw'
+ssid = 'zyxelemea'
+password = 'ZyxelEMEA20!'
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
 print("Waiting to connect:")
 while not wlan.isconnected() and wlan.status() >= 0:
     print(".", end="")   
-    time.sleep(1)
+    sleep(5)
 print("")
 print(wlan.ifconfig())
 ## define IFconfig var with ifconfig array
 IFconfig = wlan.ifconfig()
 ## take first Value of array
-IPaddress = [IFconfig[0]]
+IPaddress = IFconfig[0]
 
+##Set up Database
+db = TinyDB('db.json')
+## check if db empty, if not: clear all data
+if db.all() != []:
+    db.truncate()
 
-## define flask/picoweb
-def index(req, resp):
-    method = req.method
-    print("Method was:" + method)
-    yield from picoweb.start_response(resp)
-    yield from resp.awrite("Welcome to the Char Data API running on PiPico W and picoweb!")
-    
-def json(req, resp):
-    method = req.method
-    print("Method was:" + method)
-    
-    if req.method == "POST":
+## define picoweb/picoweb
+app = picoweb.WebApp(__name__)
+
+@app.route("/json")
+## Get the JSON Data from the chrome extension
+def json_post_data(req, resp):
+    if req.method == 'OPTIONS':
+        headers = {"Access-Control-Allow-Methods": "POST, GET", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Access-Control-Allow-Methods, Access-Control-Allow-Origin, Origin, Accept, Content-Type"}
+        yield from picoweb.start_response(resp, status="200", headers=headers)
+        method = req.method
+        ##DEBUG OPTIONS##
+        ##print("CORS HEADER RECEIVED")##
+        ##print("Method was:" + method)##
+    elif req.method == 'POST':
+        ##create vars
         size = int(req.headers[b"Content-Length"])
-        print("Size: ", size)
-        yield from req.read_form_data()
+        read_request = yield from req.reader.readexactly(size)
+        request_data = json.loads(read_request)
+        headers_json = {'Content-Type': 'application/json', 'Accept': "application/json"}
+        ## next data digestion
+        yield from picoweb.start_response(resp, content_type="application/json; charset=utf-8", status="200", headers=headers_json)
+        ##DEBUG OPTIONS##
+        ##print("DATA RECEIVED")##
+        ##print(request_data)##
+        ##method = req.method##
+        ##print("Method was:" + method)##
+        ##print("Writing Data to DB!")##
+        ##db.insert(request_data)##
     else:
-        req.parse_qs()
-        
-yield from picoweb.start_response(resp)
-yield from resp.awrite("HTTP/1.0 200 OK\r\n")
-yield from resp.awrite("Content-Type: application/json\r\n")
-yield from resp.awrite("\r\n")
+        yield from picoweb.start_response(resp, content_type = "text/html")
+        yield from app.render_template(resp, 'error.html')
+        ##DEBUG OPTIONS##
+        ##print("ERROR! NOT A VALID REQUEST!")##
+        ##method = req.method##
+        ##print("Method was:" + method)##
 
-print(req.headers)
-print(req.form)
-char_data_import = req.form
+## Documentation Endpoint
+@app.route("/")
+def index(req, resp):
+    if req.method == 'GET':
+        yield from picoweb.start_response(resp, content_type = "text/html")
+        yield from app.render_template(resp, 'index.html')
+    else:
+        yield from picoweb.start_response(resp, content_type = "text/html")
+        yield from app.render_template(resp, 'error.html')
     
-ROUTES = [
-            ("/", index),
-            ("/json", json)
-         ]
+## Configuration Endpoint
+@app.route("/config")
+def config(req, resp):
+    if req.method == 'GET':
+        yield from picoweb.start_response(resp, content_type = "text/html")
+        yield from app.render_template(resp, 'config.html')
+    else:
+        yield from picoweb.start_response(resp, content_type = "text/html")
+        yield from app.render_template('error.html')
 
-logging.basicConfig(level=logging.INFO)         
-app = picoweb.WebApp(Char_Data_API, ROUTES)
+logging.basicConfig(level=logging.DEBUG)
 app.run(debug=2, port = 80, host = IPaddress)
-    
-## Grab Posted Data from Chrome extension and convert to python dict via JSON
-json_convert_data = json.loads(char_data_import)
-python_convert_data = json.dumps(json_convert_data)
-char_data = python_convert_data
 
-## Check converted data by printing the dict
-print(char_data)
 
 #######################################################
 #######################################################
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-## EXAMPLE CODES
-#######################################################
-#######################################################
-def index(req, resp):
-    method = req.method
-    print("Method was:" + method)
-    yield from picoweb.start_response(resp)
-    yield from resp.awrite("Hello world from picoweb running on the ESP32")
-
-def batteryslave(req, resp):
-    method = req.method
-    print("Method was:" + method)
-
-    if req.method == "POST":
-        size = int(req.headers[b"Content-Length"])
-        print("Size: ", size)
-        yield from req.read_form_data()
-    else:
-        req.parse_qs()
-
-    yield from picoweb.start_response(resp)
-    yield from resp.awrite("OK")
-    yield from resp.awrite("\r\n")
-
-    print(req.headers)
-
-ROUTES = [
-            ("/", index),
-            ("/batteryslave", batteryslave)
-         ]
-         
-app = picoweb.WebApp(__name__, ROUTES)
-app.run(debug=2, port = 80, host = "192.168.42.192")
-#######################################################
-#######################################################
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def form():
-    return render_template('form.html')
-
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
-
-if __name__ == "__main__":
-    app.run()
-#######################################################
-#######################################################
-#
-# This is a picoweb example showing a centralized web page route
-# specification (classical Django style).
-#
-import ure as re
-import picoweb
-
-
-def index(req, resp):
-    # You can construct an HTTP response completely yourself, having
-    # a full control of headers sent...
-    yield from resp.awrite("HTTP/1.0 200 OK\r\n")
-    yield from resp.awrite("Content-Type: text/html\r\n")
-    yield from resp.awrite("\r\n")
-    yield from resp.awrite("I can show you a table of <a href='squares'>squares</a>.<br/>")
-    yield from resp.awrite("Or my <a href='file'>source</a>.")
-
-
-def squares(req, resp):
-    # Or can use a convenience function start_response() (see its source for
-    # extra params it takes).
-    yield from picoweb.start_response(resp)
-    yield from app.render_template(resp, "squares.tpl", (req,))
-
-
-def hello(req, resp):
-    yield from picoweb.start_response(resp)
-    # Here's how you extract matched groups from a regex URI match
-    yield from resp.awrite("Hello " + req.url_match.group(1))
-
-
-ROUTES = [
-    # You can specify exact URI string matches...
-    ("/", index),
-    ("/squares", squares),
-    ("/file", lambda req, resp: (yield from app.sendfile(resp, "example_webapp.py"))),
-    # ... or match using a regex, the match result available as req.url_match
-    # for match group extraction in your view.
-    (re.compile("^/iam/(.+)"), hello),
-]
-
-
-import ulogging as logging
-logging.basicConfig(level=logging.INFO)
-#logging.basicConfig(level=logging.DEBUG)
-
-app = picoweb.WebApp(__name__, ROUTES)
-# debug values:
-# -1 disable all logging
-# 0 (False) normal logging: requests and errors
-# 1 (True) debug logging
-# 2 extra debug logging
-app.run(debug=1)
-#######################################################
-#######################################################  
