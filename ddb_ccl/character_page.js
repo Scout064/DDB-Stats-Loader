6f28bf680cc26ff98738d154f529d02306d9accd
Scout064 <78973583+Scout064@users.noreply.github.com>
2023-03-02 14:29:58 +0100

    Update character_page.js
    Updated to inject a Start/Stop Button.
    When the button is clicked, it either starts the script and sets the timer to run every 60 seconds, or stops the timer if it's already running. The getHP() and sendHP() functions have also been separated to make the code more modular.

--- ddb_ccl/character_page.js
+++ ddb_ccl/character_page.js
@@ -1,38 +1,72 @@
 /*eslint-env es6*/
+let timerId;
+
 // define waiting function for Char Sheet loading
-            async function waitForLoading() {
-            if (!document.querySelector('.ct-character-header-info') || document.querySelector('.ct-loading-blocker')) {
-                return new Promise((r) =>
-                    setTimeout(() => {
-                        r(waitForLoading());
-                    }, 200)
-                );
-            }
-            return Promise.resolve();
-            }
-// init waiting function
-            async function init(){
-            await waitForLoading();
+async function waitForLoading() {
+  if (!document.querySelector('.ct-character-header-info') || document.querySelector('.ct-loading-blocker')) {
+    return new Promise((r) =>
+      setTimeout(() => {
+        r(waitForLoading());
+      }, 200)
+    );
+  }
+  return Promise.resolve();
+}
+
 // grab current and max HP from Character
-            const current_hp = document.querySelector('div.ct-health-summary__hp-group.ct-health-summary__hp-group--primary > div:nth-child(1) > div.ct-health-summary__hp-item-content > div').innerText;
-            const max_hp = document.querySelector('div.ct-health-summary__hp-group.ct-health-summary__hp-group--primary > div:nth-child(3) > div.ct-health-summary__hp-item-content > div').innerText;
-//            const temp_hp = document.querySelector('div.ct-health-summary__hp-group.ct-health-summary__hp-group--temp > div:nth-child(3) > div.ct-health-summary__hp-item-content > div').innerText;
-            const name = document.querySelector('h1').innerText;
+function getHP() {
+  const current_hp = document.querySelector('div.ct-health-summary__hp-group.ct-health-summary__hp-group--primary > div:nth-child(1) > div.ct-health-summary__hp-item-content > div').innerText;
+  const max_hp = document.querySelector('div.ct-health-summary__hp-group.ct-health-summary__hp-group--primary > div:nth-child(3) > div.ct-health-summary__hp-item-content > div').innerText;
+  const name = document.querySelector('h1').innerText;
+  return {
+    name,
+    current_hp,
+    max_hp
+  };
+}
+
 // log values to console and send to background.js
-//            var data_array;
-            var json_string = JSON.stringify({[new String('Name')]: name ,[new String('currentHP')]: current_hp ,[new String('maxHP')]: max_hp });
-            var data_array = JSON.parse(json_string);
-            console.log('Done'),
-            console.log('Name' + ": " + name),
-            console.log('current HP' + ": " + current_hp),
-            console.log('max HP' + ": " + max_hp),
-//            console.log('temp HP: ' + temp_hp),
-//            data_array.push("Name", ":", name),
-//            data_array.push("currentHP", ":", current_hp),
-//            data_array.push("maxHP", ":", max_hp),
-//            data_array.push('tempHP: ' + temp_hp),
-//            console.log(data_array),
-            // push URL to backgrund script
-            chrome.runtime.sendMessage({message: 'data_array', data: data_array})}
-init();
-setTimeout(function () {window.location.reload()}, 60000);
+function sendHP() {
+  const hp = getHP();
+  const data_array = {
+    Name: hp.name,
+    currentHP: hp.current_hp,
+    maxHP: hp.max_hp
+  };
+  console.log('Name' + ": " + hp.name);
+  console.log('current HP' + ": " + hp.current_hp);
+  console.log('max HP' + ": " + hp.max_hp);
+  // push data to background script
+  chrome.runtime.sendMessage({ message: 'data_array', data: data_array });
+}
+
+// initialize the script
+async function init() {
+  await waitForLoading();
+  sendHP();
+}
+
+// add start/stop button to page
+function addStartStopButton() {
+  const button = document.createElement('button');
+  button.textContent = 'Start';
+  button.style.position = 'fixed';
+  button.style.top = '10px';
+  button.style.right = '10px';
+  button.style.zIndex = '9999';
+  button.addEventListener('click', () => {
+    if (button.textContent === 'Start') {
+      init();
+      timerId = setInterval(() => {
+        init();
+      }, 60000);
+      button.textContent = 'Stop';
+    } else {
+      clearInterval(timerId);
+      button.textContent = 'Start';
+    }
+  });
+  document.body.appendChild(button);
+}
+
+addStartStopButton();
